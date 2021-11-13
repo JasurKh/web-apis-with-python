a8f895b54112b4e399d9d202d727c913cf01fa11
jasur.khojiev <khojievjasur@gmail.com>
2021-11-13 10:09:20 -0800

    all initial steps of the book

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 
-# Intitialise the app
+# Initialise the app
 app = Flask(__name__)
+
 
 # Define what the app does
 @app.get("/greet")
@@ -14,4 +15,47 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    # response = {"data": "Hello, World"}
+    # #return jsonify(response)
+    # return response
+
+    # # passing arguments to URL and returning argument in the response:
+    # name = request.args.get("name")
+    # response = {"data": f"Hello, {name}jan!"}  # this way SQL injection can be done, so next approach is better
+    #
+    # return jsonify(response)
+
+    # this way if user is sending wrong parameter our code will throw an error:
+    # name = request.args.get("name")
+    # if not name:
+    #     return jsonify({"status": "error"})
+    #
+    # response = {"data": f"Hello, {name}!!"}
+    # return jsonify(response)
+
+    # multi-argument interactive API: takes multiple arguments, in this eg name and lastname
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+
+    if not fname and not lname:
+        # if both first name and last name are missing, return an error
+        return jsonify({"status": "error"})
+
+    elif fname and not lname:
+        # if first name is present but last name is missing
+        response = {"data": f"Hello, {fname}!!"}
+
+    elif not fname and lname:
+        # if first name is missing but last name is present
+        response = {"data": f"Hello, Mr. {lname}"}
+
+    else:
+        # if none of the above is true, then both names must be present
+        response = {"data": f"Is your full name {fname} {lname}???"}
+
+    return jsonify(response)
+
+
+""" just run <flask run> in the terminal and hit end point in the terminal eg:
+curl -i -X GET "http://127.0.0.1:5000/greet?fname=Jason&lname=Statham"
+"""
